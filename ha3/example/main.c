06247e8b117adb7ddffde94546698a4fdc87825e
quarriedstone <quarriedstone@gmail.com>
2019-12-08 17:57:26 +0300

    Project: added code

--- ha3/example/main.c
+++ ha3/example/main.c
@@ -7,13 +7,13 @@ int main() {
 	blackbox_init(n);
 
 	const size_t num_steps = 1e8;
+	
 	const double h = 2.0 / num_steps;
 
-	double sum = 0;
-	const double x0 = -1 + h/2;
+	double integral = 0;
 	for (size_t i = 0; i < num_steps; i++) {
-		sum += blackbox(-1 + 0.5*h + i*h) * h;
+		integral += blackbox(-1 + 0.5*h + i*h) * h;
 	}
 
-	printf("%.12lf\n", sum);
+	printf("%.12lf\n", integral);
 }
